4f0c8405e55b6c4e3a066d15cda02a82ac8006e4
Faraji-Ismael <ismael.faraji.faraji@insjoanoliver.cat>
2025-03-25 22:24:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let nombre=prompt('Dime tu nombre completo');
-let mensaje=('Bienvenido '+nombre);
+let nombre=prompt('Per accedir, introdueix el teu nom complet: ');
+let mensaje=('Benvingut '+nombre);
 document.getElementById('boton').addEventListener('click', mostrar);
 function mostrar() {
 alert(mensaje);
